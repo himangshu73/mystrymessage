58ebf03fdf54678ae492488b441f7d4cf50bd169
himangshu <himangshu73@gmail.com>
2025-02-17 21:27:03 +0600

    get accept message status

--- app/api/accept-messages/route.ts
+++ app/api/accept-messages/route.ts
@@ -40,6 +40,7 @@ export async function POST(request: Request) {
       {
         success: true,
         message: "Accept Message Status Changed successfully.",
+        updatedUser,
       },
       { status: 200 }
     );
@@ -49,6 +50,53 @@ export async function POST(request: Request) {
       {
         success: false,
         message: "Failed to update user staus to accept messages",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      { success: false, message: "Not authenticated" },
+      { status: 500 }
+    );
+  }
+
+  const userId = user._id;
+
+  try {
+    const foundUser = await UserModel.findById(userId);
+
+    if (!foundUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 401 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        isAcceptingMessages: foundUser.isAcceptingMessage,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Error is getting accepting message status.");
+    return Response.json(
+      {
+        success: false,
+        message: "Error is getting accepting message status.",
       },
       { status: 500 }
     );
